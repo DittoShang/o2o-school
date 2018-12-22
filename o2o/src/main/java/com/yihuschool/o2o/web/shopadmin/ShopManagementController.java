bb86a1c0b623218a90d38a38910b4ee532a17b90
DittoShang <shang.d.r@qq.com>
2018-12-22 22:33:14 +0800

    181222 完成第三章

--- o2o/src/main/java/com/yihuschool/o2o/web/shopadmin/ShopManagementController.java
+++ o2o/src/main/java/com/yihuschool/o2o/web/shopadmin/ShopManagementController.java
@@ -5,7 +5,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,10 +23,15 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yihuschool.o2o.dto.ShopExecution;
+import com.yihuschool.o2o.entity.Area;
 import com.yihuschool.o2o.entity.PersonInfo;
 import com.yihuschool.o2o.entity.Shop;
+import com.yihuschool.o2o.entity.ShopCategory;
 import com.yihuschool.o2o.enums.ShopStateEnum;
+import com.yihuschool.o2o.service.AreaService;
+import com.yihuschool.o2o.service.ShopCategoryService;
 import com.yihuschool.o2o.service.ShopService;
+import com.yihuschool.o2o.util.CodeUtil;
 import com.yihuschool.o2o.util.HttpServletRequestUtil;
 import com.yihuschool.o2o.util.ImageUtil;
 import com.yihuschool.o2o.util.PathUtil;
@@ -34,11 +41,45 @@ import com.yihuschool.o2o.util.PathUtil;
 public class ShopManagementController {
 	@Autowired
 	private ShopService shopService;
+	@Autowired
+	private ShopCategoryService shopCategoryService;
+	@Autowired
+	private AreaService areaService;
+	
+	@RequestMapping(value="/getshopinitinfo",method=RequestMethod.GET)
+	@ResponseBody
+	private Map<String,Object> getShopInitInfo(){
+		Map<String,Object> modelMap=new HashMap<String,Object>();
+		List<ShopCategory> shopCategoryList=new ArrayList<ShopCategory>();
+		List<Area> areaList=new ArrayList<Area>();
+		
+		try {
+			shopCategoryList=shopCategoryService.getShopCategoryList(new ShopCategory());
+			areaList=areaService.getAreaList();
+			
+			modelMap.put("shopCategoryList", shopCategoryList);
+			modelMap.put("areaList", areaList);
+			modelMap.put("success", true);
+		}catch(Exception e) {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", e.getMessage());
+		}
+		return modelMap;
+	}
+	
+	
 	
 	@RequestMapping(value="/registershop",method=RequestMethod.POST)
 	@ResponseBody
 	public Map<String,Object> registerShop(HttpServletRequest request){
 		Map<String,Object> modelMap=new HashMap<String,Object>();
+		
+		if(!CodeUtil.checkVerifyCode(request)) {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "输入了错误的验证码");
+			return modelMap;
+		}
+		
 		//1.接受并转化相关的参数，包括店铺信息以及图片信息
 		String shopStr=HttpServletRequestUtil.getString(request, "shopStr");
 		ObjectMapper mapper=new ObjectMapper();
